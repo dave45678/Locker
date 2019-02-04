e3125d90a42ad8119bec603f845ecb8c6e08f286
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-04 17:07:03 -0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+/**
+ * Locker Problem (Arrays)
+ *
+ * Write a program to simulate the following experiment. You have 100 closed lockers. Start with the first locker and
+ * for every locker open it. Now, starting with the second locker, for this locker and every second locker after that,
+ * if the locker is open, close it; if it is closed, open it. Then, starting with the third locker, for this locker and
+ * every third locker after that, if the locker is open, close it; if it is closed, open it. Continue with the nth
+ * locker changing the “position” of every nth locker until n = 100. Print out the locker numbers that remain open.
+ * What do you notice about the lockers that remain open?
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //You have 100 closed lockers.
+	    boolean arr[] = new boolean[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j+= (i+1)) {
+                arr[j] =!arr[j];
+            }
+        }
+
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i]){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
